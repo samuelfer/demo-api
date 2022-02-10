a3c8d5f0232a9ab9000d8a07be2fa45467762872
samuelfer <samuelfesant@gmail.com>
2022-02-10 19:31:43 -0300

    Retire método responsável por salvar efetivamente do controller e coloca no service

--- src/main/java/com/marhasoft/demo/controller/ServicoPrestadoController.java
+++ src/main/java/com/marhasoft/demo/controller/ServicoPrestadoController.java
@@ -1,16 +1,12 @@
 package com.marhasoft.demo.controller;
 
 import com.marhasoft.demo.dto.ServicoPrestadoDTO;
-import com.marhasoft.demo.model.Cliente;
 import com.marhasoft.demo.model.ServicoPrestado;
-import com.marhasoft.demo.repository.ServicoPrestadoRepository;
 import com.marhasoft.demo.service.ClienteService;
-import com.marhasoft.demo.util.BigDecimalConverter;
-import com.marhasoft.demo.util.DateConverter;
+import com.marhasoft.demo.service.ServicoPrestadoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -21,25 +17,12 @@ import java.util.List;
 public class ServicoPrestadoController {
 
     private final ClienteService clienteService;
-    private final ServicoPrestadoRepository servicoPrestadoRepository;
-    private final BigDecimalConverter bigDecimalConverter;
-    private final DateConverter dateConverter;
+    private final ServicoPrestadoService servicoPrestadoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO servicoPrestadoDto) {
-        Integer clienteId = servicoPrestadoDto.getClienteId();
-        Cliente cliente = clienteService.buscarPorId(clienteId)
-                .orElseThrow(() ->
-                        new ResponseStatusException(
-                                HttpStatus.BAD_REQUEST, "Cliente inexistente no sistema!"));
-
-        ServicoPrestado servicoPrestado = new ServicoPrestado();
-        servicoPrestado.setDescricao(servicoPrestadoDto.getDescricao());
-        servicoPrestado.setData(dateConverter.converterStringEmLocalDate(servicoPrestadoDto.getData()));
-        servicoPrestado.setCliente(cliente);
-        servicoPrestado.setValor(bigDecimalConverter.converter(servicoPrestadoDto.getPreco()));
-        return servicoPrestadoRepository.save(servicoPrestado);
+        return servicoPrestadoService.salvar(servicoPrestadoDto);
     }
 
     @GetMapping
@@ -47,7 +30,7 @@ public class ServicoPrestadoController {
             @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
             @RequestParam(value = "mes", required = false) Integer mes
     ) {
-        return servicoPrestadoRepository.findByNomeClienteAndMes("%" +nome+ "%", mes);
+        return servicoPrestadoService.findByNomeClienteAndMes("%" +nome+ "%", mes);
     }
 
 }
