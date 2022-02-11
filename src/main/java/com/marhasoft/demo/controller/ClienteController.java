73007721ff7f71e90a76b86502d9c48c2873eaf3
samuelfer <samuelfesant@gmail.com>
2022-02-10 21:29:59 -0300

    Permite acessar os endpoints

--- src/main/java/com/marhasoft/demo/controller/ClienteController.java
+++ src/main/java/com/marhasoft/demo/controller/ClienteController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     private static String CLIENTE_NAO_ENCONTRADO = "Cliente não encontrado";
