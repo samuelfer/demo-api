b9e1d1161e4f33f4a2d0e7228edc2093d45121bf
samuelfer <samuelfesant@gmail.com>
2022-02-14 20:17:25 -0300

    Adicionando modelo de usuário

---
+++ src/main/java/com/marhasoft/demo/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.marhasoft.demo.repository;
+
+import com.marhasoft.demo.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
